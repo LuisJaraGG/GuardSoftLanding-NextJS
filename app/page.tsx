50d199ef6a829ffa06381c6a325e2f85e1522280
Luis albarran <luisalbarran21rl04091216@email.com>
2023-09-12 19:18:56 -0500

    Primer Avance, modificacion-carga-perezosa

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import HeroPage from "@/components/heroPage";
-import About from "@/components/about";
-import Purpose from "@/components/purpose";
-import Services from "@/components/services";
-import Proyects from "@/components/proyects";
-import LocateUs from "@/components/locateUs";
-import Contact from "@/components/contact";
+
+import dynamic from "next/dynamic";
+const Purpose = dynamic(() => import("@/components/purpose"));
+const Services = dynamic(() => import("@/components/services"));
+const Proyects = dynamic(() => import("@/components/proyects"));
+const LocateUs = dynamic(() => import("@/components/locateUs"));
+const Contact = dynamic(() => import("@/components/contact"));
+
+const About = dynamic(() => import("@/components/about"));
+
 
 export default function Home() {
   return (
