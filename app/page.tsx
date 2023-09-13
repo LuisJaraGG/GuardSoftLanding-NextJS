bd94c2b4ed4680e507629dbc1c27a9ab0a61fc05
Luis albarran <luisalbarran21rl04091216@email.com>
2023-09-12 19:40:03 -0500

    Primer Avance, modificacion-ssr:false

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,22 @@
 import HeroPage from "@/components/heroPage";
 
 import dynamic from "next/dynamic";
-const Purpose = dynamic(() => import("@/components/purpose"));
-const Services = dynamic(() => import("@/components/services"));
-const Proyects = dynamic(() => import("@/components/proyects"));
-const LocateUs = dynamic(() => import("@/components/locateUs"));
-const Contact = dynamic(() => import("@/components/contact"));
+const Purpose = dynamic(() => import("@/components/purpose"),{ ssr: false });
+const Services = dynamic(() => import("@/components/services"), { ssr: false });
+const Proyects = dynamic(() => import("@/components/proyects"), { ssr: false });
+const LocateUs = dynamic(() => import("@/components/locateUs"), { ssr: false });
+const Contact = dynamic(() => import("@/components/contact"), { ssr: false });
 
 const About = dynamic(() => import("@/components/about"));
+// import { useState } from "react";
 
 
 export default function Home() {
+  // const [showPurpose, setshowPurpose] = useState(false)
+  // const [showServices, setshowServices] = useState(false);
+  // const [showProyects, setshowProyects] = useState(false);
+  // const [showLocateUs, setshowLocateUs] = useState(false);
+  // const [showContact, setshowContact] = useState(false);
   return (
     <main className="">
       <HeroPage></HeroPage>
